df0f93a9ce22291843177e59f366966fc5e84c03
Jialong Zhang <z841063918@gmail.com>
2019-08-09 14:54:51 -0400

    addLocation

---
+++ src/app/add-location/add-location.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+
+@Component({
+  selector: 'dev-add-location',
+  templateUrl: './add-location.component.html',
+  styleUrls: ['./add-location.component.scss']
+})
+export class AddLocationComponent implements OnInit {
+
+  location: Location;
+  constructor(private datasvc: ApiService) { }
+  
+
+  ngOnInit() {
+  }
+
+  postRoomInfo(value: Location){
+    console.log(value);
+    
+     this.datasvc.postLocationData(value).subscribe(data => {
+      //post location success
+      console.log(data);
+      console.log("Post success");
+
+    }), error => {
+      //post location error handling 
+      console.log("Error", error);
+      console.log(value);
+    } 
+    //console.log(value);
+  }
+
+}
