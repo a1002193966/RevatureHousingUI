8fea70d1eaef0738a34f4ded75650b2a794bbee9
samli39 <samli_39@hotmail.com>
2019-08-09 13:34:55 -0400

    finish app.component testing

--- src/app/nav/nav.component.spec.ts
+++ src/app/nav/nav.component.spec.ts
@@ -1,25 +1,53 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavComponent } from './nav.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { RouterLinkDirectiveStub } from '../testing/router-link-directive-stub';
+import { By } from '@angular/platform-browser';
 
 describe('NavComponent', () => {
   let component: NavComponent;
   let fixture: ComponentFixture<NavComponent>;
+  let compiled: any;
+  let linkDes: any;
+  let routerLinks: any;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NavComponent ]
+      declarations: [
+        NavComponent,
+        RouterLinkDirectiveStub
+      ],
+      imports: [
+        RouterTestingModule
+      ],
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(NavComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    compiled = fixture.debugElement.nativeElement;
+
+    // find DebugElements with an attached RouterLinkStubDirective
+    linkDes = fixture.debugElement
+      .queryAll(By.directive(RouterLinkDirectiveStub));
+
+    // get attached link directive instances
+    // using each DebugElement's injector
+    routerLinks = linkDes.map(de => de.injector.get(RouterLinkDirectiveStub));
+
   });
 
-  it('should create', () => {
+  it('should create nav component', () => {
     expect(component).toBeTruthy();
   });
+
+  // it('can get RouterLinks from page', () => {
+  //   expect(routerLinks.length).toBe(4, 'should have 4 routerLinks');
+  //   expect(routerLinks[0].linkParams).toBe('/order');
+  //   expect(routerLinks[1].linkParams).toBe('/creditCard');
+  // });
 });
