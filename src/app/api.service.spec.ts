aece2506fe70e356b5485fc2bf374f4dce0b0724
samli39 <samli_39@hotmail.com>
2019-08-09 10:16:25 -0400

    fixed error from default test case

--- src/app/api.service.spec.ts
+++ src/app/api.service.spec.ts
@@ -1,12 +1,23 @@
-import { TestBed } from '@angular/core/testing';
-
+import { TestBed, getTestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { ApiService } from './api.service';
 
 describe('ApiService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
-
-  it('should be created', () => {
-    const service: ApiService = TestBed.get(ApiService);
-    expect(service).toBeTruthy();
+  let injector: TestBed;
+  let service: ApiService;
+  let httpMock: HttpTestingController;
+  beforeEach(() =>{
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [ApiService]
+    });
+  injector = getTestBed();
+  service = injector.get(ApiService);
+  httpMock = injector.get(HttpTestingController);  
   });
+
+it('should be created', () => {
+  const service: ApiService = TestBed.get(ApiService);
+  expect(service).toBeTruthy();
+});
 });
