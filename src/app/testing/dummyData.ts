e04f25129994bfb698621ed9528e3d94bc8975e8
brittanyabrahams2 <babrahams@albany.edu>
2019-08-19 17:16:26 -0400

    add room

--- src/app/testing/dummyData.ts
+++ src/app/testing/dummyData.ts
@@ -1,164 +1,328 @@
 import { ProviderLocation } from 'src/Entities/location';
+
 import { Room } from 'src/Entities/room';
 
+
+
 export const LocationData = {
+
     Address: "abc ",
+
     State: "NY",
+
     City: "city",
+
     ZipCode: "12345",
+
     //need change, might not need this properties
+
     TrainingCenter: "Queen"
+
 }
+
+
 
 export const LocationObject={
+
     locationID: 3, 
+
     address: '123 Main Street',
+
     city: 'Queens', 
+
     state: 'New York',
+
     zip: '11111',
+
     trainingCenter: 'Queens College',
+
     providerID: 1,
+
 }
+
+
 
 export const RoomData = {
+
   
+
     Type:"apartment",
+
     MaxOccupancy:2 ,
+
     RoomNumber:"1",
+
     Gender:"F",
+
     StartDate: new Date('2019/08/10'),
+
     EndDate: undefined,
+
     Description : ''
 
+
+
 }
+
+
 
 export const UpdateRoomData = {
+
   
+
     Type:"apartment",
+
     MaxOccupancy:2 ,
+
     CurrentOccupancy:2,
+
     RoomNumber:"1",
+
     Gender:"F",
+
     StartDate: new Date('2019/08/10'),
+
     EndDate: undefined,
+
     Description : '',
+
     LocationData:2
 
+
+
 }
+
+
 
 export const LocationErrorList = {
+
     ErrorList: ["Address is required",
+
         "State is required",
+
         "City is required",
+
         "Zip Code is required",
+
         "Training Center is required"
+
     ]
+
 }
 
+
+
 export const RoomErrorList={
+
     ErrorList:["Room Type is required",
+
         "Number of Beds is required",
+
         "Room Number is required",
+
         "Gender is required",
+
         "StartDate is required"
+
         
+
        
+
       
+
     ]
+
 }
+
+
+
 
 
 export const UpdateRoomErrorList={
+
     ErrorList:["Room Type is required",
+
         "Number of Beds is required",
+
         "Number of Beds is required",
+
         "Room Number is required",
+
         "Gender is required",
+
         "StartDate is required",
+
         "EndDate is required"
+
         
+
        
+
       
+
     ]
+
 }
+
+
 
 export class MockProvider extends ProviderLocation{
+
     LocationID= 3; 
+
     address= '123 Main Street';
+
     city= 'Queens'; 
+
     state= 'NY';
+
     zipCode= '11111';
+
     trainingCenter= 'Queens College';
+
     ProviderID= 1;
+
 }
+
+
 
 export class MockRoom extends Room{
+
      
 
+
+
     //Primary Key
+
     RoomID = 1;
+
     Type = 'dorm';
+
     MaxOccupancy = 4;
+
     RoomNumber = '101';
+
     Gender = 'male';
+
     StartDate = new Date('2019/08/10');
+
     EndDate =  new Date('2019/11/11');
+
     CurrentOccupancy = 2;
+
     IsActive = true;
+
     Description = 'this is a mock room';
 
+
+
     //Foreign Key
+
     LocationID = 1;
+
 }
+
+
 
 const location1 = new MockProvider();
+
 const location2 = {
+
     LocationID: 4,
+
     address:'65 Kissena',
+
     city: 'New York',
+
     state: 'NY',
+
     zipCode: '10000',
+
     trainingCenter: 'Queens College',
+
     ProviderID: 1
+
 }
+
 export const mockLocationList = [{location1}, {location2}]
 
+
+
 const room1 = new MockRoom();
+
 const room2= {
+
      //Primary Key
+
      RoomID : 2,
+
      Type : 'dorm',
+
      MaxOccupancy : 4,
+
      RoomNumber : '102',
+
      Gender : 'female',
+
      StartDate : new Date('2019/08/10'),
+
      EndDate :  new Date('2019/11/11'),
+
      CurrentOccupancy : 2,
+
      IsActive : true,
+
      Description : 'this is a mock room',
+
  
+
      //Foreign Key
+
      LocationID : 1
+
 }
+
 export const mockRoomList = [{room1}, {room2}]
+
+
 
 export const RoomObject={
 
+
+    
+
      
 
+
+
     //Primary Key
+
     roomID : 1,
+
     type : 'dorm',
+
     maxOccupancy : 4,
+
     roomNumber : '101',
+
     gender : 'male',
+
     startDate : new Date('2019/08/10'),
+
     endDate :  new Date('2019/11/11'),
+
     currentOccupancy : 2,
+
     isActive : true,
+
     description : 'this is a mock room',
 
+
+
     //Foreign Key
+
     locationID : 1
+
+
 
 }
--- src/app/update-room/update-room.component.spec.ts
+++ src/app/update-room/update-room.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { LocationObject, RoomObject, UpdateRoomData, } from '../testing/dummyData';
+import { LocationObject, RoomObject, UpdateRoomData, MockRoom, } from '../testing/dummyData';
 import  { UpdateRoomComponent } from './update-room.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -12,6 +12,7 @@ import { formatDate, DatePipe} from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { of } from 'rxjs';
 import { Router } from '@angular/router';
+import { Room } from 'src/Entities/room';
 
 
 
@@ -20,6 +21,13 @@ describe('UpdateRoomComponent', () => {
   let fixture: ComponentFixture<UpdateRoomComponent>;
   let errorList: string[];
 
+  function formSetup(){
+    component.mygroup.controls['Type'].setValue(RoomData.Type);
+    component.mygroup.controls['MaxOccupancy'].setValue(RoomData.MaxOccupancy);
+    component.mygroup.controls['RoomNumber'].setValue(RoomData.RoomNumber);
+    component.mygroup.controls['Gender'].setValue(RoomData.Gender);
+    component.mygroup.controls['StartDate'].setValue(RoomData.StartDate);
+  }
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ UpdateRoomComponent],
@@ -53,25 +61,62 @@ describe('UpdateRoomComponent', () => {
   });
 
 //onNgInit()
-it('should initialize formgroup by calling onNgInit()', () => {
-  
+it('should call getRoominfo on ngOnInit and setup the form', () => {
+ formSetup();
+  spyOn(component,'getRoomInfo');
+
   component.ngOnInit();
 
     expect(component.mygroup.controls['Type'].value).toBe("");
     expect(component.mygroup.controls['MaxOccupancy'].value).toBe("");
+    expect(component.mygroup.controls['CurrentOccupancy'].value).toBe("");
     expect(component.mygroup.controls['RoomNumber'].value).toBe("");
     expect(component.mygroup.controls['Gender'].value).toBe("");
     expect(component.mygroup.controls['StartDate'].value).toBe("");
-})
-//  //#region html
+    expect(component.mygroup.controls['EndDate'].value).toBe("");
+    expect(component.getRoomInfo).toHaveBeenCalled();
 
-//  it('click update button should call updateroom with roomID to', () => {
-//   spyOn(component, 'onSubmit');
-//   const button = fixture.debugElement.query(By.css('button[type=submit]')).nativeElement;
-//   button.click();
-//   expect(component.onSubmit).toHaveBeenCalled();
-// })
-// //#endregion
+})
+
+//assignRoomId
+it('should assign id',() =>{
+  component.roomId=null;
+  component.assignRoomId(1);
+  expect(component.roomId).toEqual(1);
+
+})
+
+  //getRoomInfo()
+  it('should assign value to room',()=>{
+    //reset value
+    component.room=null;
+    component.getRoomInfo();
+    expect(component.room).toEqual(RoomObject);
+    
+  })
+
+  //updateRoom()
+  it('should call updateroomdata in service',()=>{
+    const xService = fixture.debugElement.injector.get(ApiService);
+    spyOn(xService,'updateRoomData').and.returnValue(of());
+  
+   const test = new MockRoom();
+//
+   component.updateRoomInfo(test);
+
+     expect(xService.updateRoomData).toHaveBeenCalledWith(test);
+
+  })
+
+ //#region html
+
+ it('click update button should call updateroom with roomID to', () => {
+  spyOn(component, 'onSubmit');
+  const button = fixture.debugElement.query(By.css('button[type=submit]')).nativeElement;
+  button.click();
+  expect(component.onSubmit).toHaveBeenCalled();
+})
+//#endregion
 
 // //onNgInit()
 // it('should initialize formgroup by calling onNgInit()', () => {
@@ -93,13 +138,6 @@ it('should initialize formgroup by calling onNgInit()', () => {
 // })
 
 
-//assignRoomId
-// it('should assign id',() =>{
-//   component.roomId=null;
-//   component.assignRoomId(1);
-//   expect(component.roomId).toEqual(1);
-
-// })
 
 //  //getRoomInfo()
 //  it('should assign value to room',()=>{
